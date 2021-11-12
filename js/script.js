4d3f31da1fdfd607a2447a4118b27d3651bac6f2
Glitch (oasis-indecisive-mapusaurus) <none>
2021-11-12 01:08:13 +0000

    11/11/21

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-
+$(".play").click(function(){
+  var userChoice = $(".input").val();
+  var computerChoice = $("computerChoice").val();
+  var random = (Math.random()*10);
+  console.log(random);
+  $(".userChoice").text(userChoice);
+  
+  if (random >=0 && random < (10/3)){
+    $(".computerChoice").text("rock");
+    computerChoice = "rock";
+  }else if(random >= (10/3) && random <= 2*(10/3)){
+    $(".computerChoice").text("paper");
+    computerChoice = "paper";
+  }else if(random > 2*(10/3) && random < 10){
+    $(".computerChoice").text("scissors");
+    computerChoice = "scissors";
+  }
+  
+  if(userChoice === computerChoice){
+    $(".result").text("TIE");
+  }else if(userChoice === "rock" && computerChoice === "scissors"){
+    $(".result").text("User wins");
+  }else if(userChoice === "scissors" && computerChoice === "rock"){
+    $(".result").text("Computer wins");
+  }else if(userChoice === "paper" && computerChoice === "scissors"){
+    $(".result").text("Computer wins");
+  }else if(userChoice === "scissors" && computerChoice === "paper"){
+    $(".result").text("User wins");
+  }else if(userChoice === "rock" && computerChoice === "paper"){
+    $(".result").text("Computer wins");
+  }else if(userChoice === "paper" && computerChoice === "rock"){
+    $(".result").text("User wins");
+  }
+  
+});
